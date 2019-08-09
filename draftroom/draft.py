a31f9bca32acd7fff34a391d9eda86dd5c39ce73
fliljeda <fliljeda@kth.se>
2019-08-09 22:19:06 +0000

    restructure file hierarchy

--- draftroom/draft.py
+++ draftroom/draft.py
@@ -26,7 +26,14 @@ if(inp != ""):
 print("Fetching and parsing static information...")
 static_info = fetch_fpl_json("/api/bootstrap-static")
 details = fetch_fpl_json(f"/api/league/{league_id}/details")
+choices_global = fetch_fpl_json(f"/api/draft/{league_id}/choices")["choices"]
 print("Done!")
+
+# timestamp used to avoid fetching choices when unnecessary
+timestamp = time.time()
+def update_timestamp():
+    global timestamp
+    timestamp = time.time()
  
 #CHOICES: entry->team.entry_id element->static.elements[*-1].id 
 def league_team_names():
@@ -67,48 +74,74 @@ def player_obj_fromid(player_id):
     m_player["team_code"] = team_code_fromid(player["team"])
     return m_player
 
-def choices():
-    choices = fetch_fpl_json(f"/api/draft/{league_id}/choices")
-    league_teams = league_team_names()
-    for pick in choices["choices"]:
-        if pick["choice_time"] is None:
-            break
-        player_name = player_obj_fromid(pick["element"])
-        league_teams[pick["entry_name"]].append(player_name)
-    return league_teams
 
-async def update_choices(websocket, path):
+# Sends the picked players over the websocket
+async def serve_picks(websocket, path):
     print("Connection");
     league_teams = league_team_names()
+
+    # Send initial, will provide the teams even if no picks has been made yet
     await websocket.send(json.dumps({"action":"update", "picks":league_teams}))
+
     pick_index = 0
     while True:
-        choices = fetch_fpl_json(f"/api/draft/{league_id}/choices")["choices"]
+        #choices = fetch_fpl_json(f"/api/draft/{league_id}/choices")["choices"]
+        ts = await websocket.ping()
+        await ts
+        print("Ok")
+
+        #Update the global timestamp to 
+        update_timestamp()
+
         league_teams_update = league_team_names()
         newpicks = False
-        while pick_index < len(choices):
-            pick = choices[pick_index]
+
+        # Continue from last checked index to not send duplicates
+        while pick_index < len(choices_global):
+
+            # Choices obj is global cache of choices fetched elsewhere
+            pick = choices_global[pick_index]
+
+            # Stop when choice has not been made
             if pick["choice_time"] is None:
-                print("No new choices")
                 break
             else:
-                print("Found next pick")
                 pick_index += 1
                 player_name = player_obj_fromid(pick["element"])
-                league_teams[pick["entry_name"]].append(player_name)
-                league_teams_update[pick["entry_name"]].append(player_name)
+                team_entry_name = pick["entry_name"]
+                league_teams[team_entry_name].append(player_name)
+                league_teams_update[team_entry_name].append(player_name)
                 newpicks = True
+
+        # If one or more non-duplicate picks have been found, send message to client
         if newpicks:
-            print("Sending through websocket..")
-            message = {"action":"update", "picks":league_teams_update}
+            message = {
+                    "action":"update", 
+                    "picks":league_teams_update
+                    }
             await websocket.send(json.dumps(message))
-        await asyncio.sleep(5)
+
+        # Sleep to avoid overload
+        await asyncio.sleep(1)
     return league_teams
 
-#print(choices())
+
+async def update_choices():
+    global timestamp
+    last_timestamp = timestamp
+    while True:
+        print("checking timestamp..")
+        if last_timestamp < timestamp:
+            print("Timestamp newer, fetching choices")
+            choices_global = fetch_fpl_json(f"/api/draft/{league_id}/choices")["choices"]
+            last_timestamp = timestamp
+        await asyncio.sleep(5)
 
 
+update_task = asyncio.get_event_loop().create_task(update_choices())
+start_server = websockets.serve(serve_picks, 'localhost', 8008)
 
-start_server = websockets.serve(update_choices, 'localhost', 8008)
 asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_until_complete(update_task)
+
 asyncio.get_event_loop().run_forever()
